4134924732f7813a01ff08167462723c44b11772
Chandrashekhar kumar <pachandrashekhar550@gmail.com>
2026-06-24 18:13:58 +0530

    update UI

--- src/main/java/com/ai_resume_shortlisting/invogent/Controller/HealthController.java
+++ src/main/java/com/ai_resume_shortlisting/invogent/Controller/HealthController.java
@@ -1,5 +1,13 @@
 package com.ai_resume_shortlisting.invogent.Controller;
 
-public class HealthController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HealthController {
+	
+	 @GetMapping("/health")
+	    public String health() {
+	        return "AI Resume ATS Scanner Running";
+	    }
 }
